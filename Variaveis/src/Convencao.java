1abf444c183e4c3e8a8119baa7909c97efcf91f9
Gleyson Willian Bruno Costa Rocha <gleysonwillian14@gmail.com>
2025-02-18 18:23:39 -0300

    maratona java

---
+++ Variaveis/src/Convencao.java
@@ -0,0 +1,17 @@
+// Tipos primitivos:
+/* byte
+ * short
+ * long
+ * boolean
+ * int
+ * double
+ * float
+*/
+
+public class Convencao {
+    public static void main(String[] args) {
+        int idade = 10;
+
+        System.out.println("A idade digitada é de " + idade + " anos.");
+    }
+}
---
+++ src/Aula00.java
@@ -0,0 +1,5 @@
+public class Aula00 {
+    public static void main(String[] args) {
+        System.out.println("Olá, javeiros. Marcha!!!");
+    }
+}
