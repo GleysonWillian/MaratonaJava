40cbb4e1875c7ef1a2af529081a2845891a33e5f
Gleyson Willian Bruno Costa Rocha <gleysonwillian14@gmail.com>
2025-02-19 16:46:15 -0300

    commit exercicio

---
+++ src/br/devdojo/variaveis/exercicios.java
@@ -0,0 +1,13 @@
+package br.devdojo.variaveis;
+
+public class exercicios {
+    public static void main(String[] args) {
+        String nome = "Quico";
+        String endereco = "Av. JK";
+        double salario = 7854.29;
+        String dataRecebimentoSalario = "25/03/2024";
+        //String relatorio = "Eu, " + nome + ", morando no " + endereco + ", confirmo que recebi o de " + salario + " na data " + dataRecebimentoSalario;
+
+        System.out.println("Eu, " + nome + ", morando no " + endereco + ", confirmo que recebi o salário no valor de R$" + salario + " reais na data " + dataRecebimentoSalario);
+    }
+}
