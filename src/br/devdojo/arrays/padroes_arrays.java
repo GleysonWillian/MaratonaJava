d5faefd5fc38bd1c6519b79de7c6d8598917c2fd
Gleyson Willian Bruno Costa Rocha <gleysonwillian14@gmail.com>
2025-03-12 19:31:36 -0300

    Arrays pt1 Java

---
+++ src/br/devdojo/arrays/padroes_arrays.java
@@ -0,0 +1,16 @@
+package br.devdojo.arrays;
+
+public class padroes_arrays {
+    public static void main(String[] args) {
+        /*padrão de inicialização:
+         * byte, short, int, long, float e double = 0
+         * char = 'u0000'' '
+         * boolean = false
+         * String = null
+         */
+        String [] idades = new String[3];
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
