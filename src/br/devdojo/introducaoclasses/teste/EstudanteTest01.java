b2ce160f690777e42807c9fbbdd1adc4addbc23b
Gleyson Willian Bruno Costa Rocha <gleysonwillian14@gmail.com>
2025-03-17 20:04:27 -0300

    Introdução a orientação a objetos

---
+++ src/br/devdojo/introducaoclasses/teste/EstudanteTest01.java
@@ -0,0 +1,16 @@
+package br.devdojo.introducaoclasses.teste;
+
+import br.devdojo.introducaoclasses.dominio.Estudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome = "Marcos";
+        estudante.idade = 21;
+        estudante.sexo = 'M';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+    }
+}
